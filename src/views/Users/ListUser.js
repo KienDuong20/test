4e00238644a5c63cec12392ecfe8c0d34dd13337
KienDuong20 <duongtukien111020@gmail.com>
2022-03-04 23:33:48 +0700

    test

--- src/views/Users/ListUser.js
+++ src/views/Users/ListUser.js
@@ -25,7 +25,7 @@ class ListUser extends React.Component {
         return (
             <div className="list-user-container">
                 <div className="title">
-                    Fetch all list users
+                    Fetch all list users akka
                 </div>
                 <div className="list-user-content">
                     {listUsers && listUsers.length > 0 &&
